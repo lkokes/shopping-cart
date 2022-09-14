40355d5dcc10807a9ce3ebfdcd70776abc0aadb3
Lucia Kokesova <lkokesova@gmail.com>
2022-09-14 20:26:48 +0200

    Created app with navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+export function Navbar() {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-lg mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <FontAwesomeIcon icon={faCartShopping} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.2rem",
+              height: "1.2rem",
+              position: "absolute",
+              bottom: "5px",
+              right: "115px",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+}
